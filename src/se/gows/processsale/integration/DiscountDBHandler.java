dba7c5baf4a9ab4bc3f0bbc37004283e15997d0d
Sparfbag <wilmer.thorn@gmail.com>
2025-04-16 14:33:06 +0200

    Discounthandler skapad
    Preliminär discount hanterare, saknar möjlighet att plocka ut totala priset på Sale och att gå igenom listan av items

--- src/se/gows/processsale/integration/DiscountDBHandler.java
+++ src/se/gows/processsale/integration/DiscountDBHandler.java
@@ -1,5 +1,27 @@
 package se.gows.processsale.integration;
+import se.gows.processsale.model.Sale;
 
 public class DiscountDBHandler {
     
+
+
+    public double fetchDiscount(int customerID, Sale currentSale){
+
+        double discountRate = customerDiscount(customerID);
+        discountRate = discountRate*itemDiscounts(currentSale);
+
+        discountRate = discountRate*totalSaleDiscount(currentSale);
+
+        return discountRate;
+    }
+    private double itemDiscounts(Sale currentSale){
+        return 0.8;
+    }
+    private double customerDiscount(int customerID){
+        return 0.8;
+    }
+    private double totalSaleDiscount(Sale currentSale){
+        return currentSale.getTotal;
+    }
+
 }
