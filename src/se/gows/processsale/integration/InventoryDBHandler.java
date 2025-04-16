96550bc0f959e9f7af722260964228dbc4f55a3f
simzap91 <115180756+simzap91@users.noreply.github.com>
2025-04-16 14:50:00 +0200

    InventoryDBHandler comments
    Proper comments added to class.

--- src/se/gows/processsale/integration/InventoryDBHandler.java
+++ src/se/gows/processsale/integration/InventoryDBHandler.java
@@ -4,22 +4,30 @@ import se.gows.processsale.DTO.ItemDTO;
 
 public class InventoryDBHandler {
 
-    // TestDB
-    String[] testDB = {"1, mjölk, 14.9, 20", "2, smör, 39.9, 20"};
+    /*
+     * Item database for testing
+     * Contains two items
+     */
+    String[] itemtDBTest = {"1, mjölk, 14.9, 20", "2, smör, 39.9, 20"};
 
     ItemDTO fetchItemFromDB(int itemID) {
 
-        // Fetch data from testDB
+        /*
+         * Procedure in 2 steps that fetches item from itemtDBTest
+         */
         // 1) Split the first string on commas
-        String[] parts = testDB[0].split(",");
+        String[] parts = itemtDBTest[0].split(",");
+
+        // 2) Add item values to test variables
         String testDesc = parts[1].trim();
         double testPrice = Double.parseDouble(parts[2].trim());
         int testVAT = Integer.parseInt(parts[3].trim());
 
-        // Create new ItemDTO
+        /*
+         * Create new ItemDTO from fetched item data
+         */
         ItemDTO scannedItem = new ItemDTO(itemID, testDesc, testPrice, testVAT);
 
-        // Return scannedItem
         return scannedItem;
     }
 }
