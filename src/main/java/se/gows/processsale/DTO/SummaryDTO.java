d9e73ea19eb7e2d37d22542c691cf5de1124512e
simzap91 <115180756+simzap91@users.noreply.github.com>
2025-04-28 16:47:13 +0200

    Added SummaryDTO

---
+++ src/main/java/se/gows/processsale/DTO/SummaryDTO.java
@@ -0,0 +1,21 @@
+package se.gows.processsale.DTO;
+
+import java.time.LocalTime;
+
+import se.gows.processsale.model.RegisteredItem;
+
+public class SummaryDTO {
+    public LocalTime timeOfSale;
+    public double totalPrice;
+    public double totalVAT;
+    public double totalIncVat;
+    public RegisteredItem[] itemList;
+
+    public SummaryDTO (LocalTime timeOfSale, double totalPrice, int totalVAT, double totalIncVat, RegisteredItem[] itemList){
+        this.timeOfSale = timeOfSale;
+        this.totalPrice = totalPrice;
+        this.totalVAT = totalVAT;
+        this.totalIncVat = totalIncVat;
+        this.itemList = itemList;
+    }
+}
