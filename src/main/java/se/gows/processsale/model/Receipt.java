8d16526750821301e697fd1516118cd69e756242
Oscar Dalne <oscar.dalne@gmail.com>
2025-04-28 17:53:28 +0200

    Unpacking in receipt

--- src/main/java/se/gows/processsale/model/Receipt.java
+++ src/main/java/se/gows/processsale/model/Receipt.java
@@ -1,16 +1,28 @@
 package se.gows.processsale.model;
 
+import java.time.LocalTime;
+
 import se.gows.processsale.DTO.SummaryDTO;
 import se.gows.processsale.model.Transaction;
 
 
 public class Receipt {
-    private SummaryDTO summaryOfSale;
-    private Transaction trans;
+    private LocalTime timeOfSale;
+    private double totalPrice;
+    private double totalVAT;
+    private double totalIncVat;
+    private RegisteredItem[] itemList;
+    private Amount amountPaid;
+    private Amount amountChange;
     
 
     public Receipt(SummaryDTO summaryDTO, Transaction trans) {
-        this.summaryOfSale = summaryDTO;
-        this.trans = trans;
+        this.LocalTime = summaryDTO.timeOfSale;
+        this.totalPrice = summaryDTO.totalPrice;
+        this.totalVAT = summaryDTO.totalVAT;
+        this.totalIncVat = summaryDTO.totalIncVat;
+        this.itemList = summaryDTO.itemList;
+        this.amountPaid = trans.amountPaid;
+        this.amountChange = trans.amountChange;
     }
 }
