ba02297c6a6a75ae6d44c331adea9340d763ca9f
Sparfbag <wilmer.thorn@gmail.com>
2025-04-28 11:06:37 +0200

    Create Transaction.java

---
+++ src/main/java/se/gows/processsale/model/Transaction.java
@@ -0,0 +1,18 @@
+package se.gows.processsale.model;
+
+public class Transaction {
+    private Amount amountChange;
+    private double totalPrice;
+
+    private Amount calculateChange(Amount amountPaid,double totalPrice){
+        Amount change = new Amount(amountPaid.getValue() - totalPrice);
+        return change;
+    }
+
+    public Transaction(Amount amountChange, double totalPrice){
+        this.amountChange = calculateChange(amountChange, totalPrice);
+        this.totalPrice = totalPrice;
+    }
+
+    
+}
