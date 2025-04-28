3ea53bec359eb7b2d6604a94e3acbf0bba9f9f4f
Oscar Dalne <oscar.dalne@gmail.com>
2025-04-28 11:07:26 +0200

    Create discountDTO.java
    Added discountDTO

---
+++ src/main/java/se/gows/processsale/DTO/discountDTO.java
@@ -0,0 +1,14 @@
+package se.gows.processsale.DTO;
+
+public class discountDTO {
+    double discountRateSalePrice;
+    double discountIteminsale;
+    double discountRateCustomer;
+
+
+    public discountDTO(double discountRateSalePrice, double discountIteminsale, double discountRateCustomer) {
+        this.discountRateSalePrice = discountIteminsale;
+        this.discountIteminsale = discountIteminsale;
+        this.discountRateCustomer = discountRateCustomer;
+    }
+}
