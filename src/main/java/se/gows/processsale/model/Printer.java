58fc453fea5cb33a87f205e52b1eb30ef455cd27
Oscar Dalne <oscar.dalne@gmail.com>
2025-04-29 12:31:34 +0200

    added printer arts

--- src/main/java/se/gows/processsale/model/Printer.java
+++ src/main/java/se/gows/processsale/model/Printer.java
@@ -19,7 +19,8 @@ public class Printer {
         System.out.println();
         System.out.println("Total price: " + receipt.totalPrice + "kr");
         System.out.println("Total VAT: " + receipt.totalVAT + "kr");
-        System.out.println();
-        System.out.println("Total (incl. VAT):" + receipt.totalIncVat + "kr");
+        System.out.println("-------------------------------------");
+        System.out.println("Total (incl. VAT): " + String.format("%.2f", receipt.totalIncVat) + " kr");
+        System.out.println("-------------------------------------");
     }
 }
