e6225f3369876a4781e18b49af5df8d7ce3a3b3f
Sparfbag <wilmer.thorn@gmail.com>
2025-04-30 15:18:50 +0200

    jag inte alltid skriva något här

--- src/test/java/se/gows/processsale/integration/InventoryDBHandlerTest.java
+++ src/test/java/se/gows/processsale/integration/InventoryDBHandlerTest.java
@@ -1,1 +1,23 @@
 package se.gows.processsale.integration;
+import se.gows.processsale.integration.InventoryDBHandler;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class InventoryDBHandlerTest {
+    private InventoryDBHandler testInvHandler;
+    private double testID;
+    @BeforeEach
+    void setUp(){
+        testInvHandler = new InventoryDBHandler();
+        testID = 1;
+    }
+    @Test
+    void testFetchItemFromDB() {
+        
+    }
+
+    @Test
+    void testUpdateInventoryDB() {
+
+    }
+}
