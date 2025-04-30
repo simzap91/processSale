a0aa3e5e070da53aa239c47c5b7a483df6be47cf
Sparfbag <wilmer.thorn@gmail.com>
2025-04-30 13:43:04 +0200

    Fler tester

---
+++ src/test/java/se/gows/processsale/integration/DiscountDBHandlerTest.java
@@ -0,0 +1,10 @@
+package se.gows.processsale.integration;
+
+import org.junit.jupiter.api.Test;
+
+public class DiscountDBHandlerTest {
+    @Test
+    void testFetchDiscount() {
+
+    }
+}
---
+++ src/test/java/se/gows/processsale/startup/MainTest.java
@@ -0,0 +1,10 @@
+package se.gows.processsale.startup;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    void testMain() {
+
+    }
+}
