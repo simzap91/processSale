b6226bd54f9dc2beb2920cc73184f277e7cdf376
Sparfbag <wilmer.thorn@gmail.com>
2025-04-30 15:54:08 +0200

    AMOUNT TEST

--- src/test/java/se/gows/processsale/model/AmountTest.java
+++ src/test/java/se/gows/processsale/model/AmountTest.java
@@ -1,10 +1,18 @@
 package se.gows.processsale.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class AmountTest {
+    private Amount testAmount;
+    private double testValue;
     @Test
     void testGetValue() {
-
+        testValue = 100;
+        testAmount = new Amount(testValue);
+        boolean expectedResult = true;
+        boolean result = testValue == testAmount.getValue();
+        assertEquals(expectedResult, result, "Result should be equal");
     }
 }
