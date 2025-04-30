5dc93166373c1a910679ff7abf7f42ee9ced9986
Sparfbag <wilmer.thorn@gmail.com>
2025-04-30 14:57:35 +0200

    tester

---
+++ src/test/java/se/gows/processsale/model/AmountTest.java
@@ -0,0 +1,10 @@
+package se.gows.processsale.model;
+
+import org.junit.jupiter.api.Test;
+
+public class AmountTest {
+    @Test
+    void testGetValue() {
+
+    }
+}
---
+++ src/test/java/se/gows/processsale/model/RegisteredItemTest.java
@@ -0,0 +1,25 @@
+package se.gows.processsale.model;
+
+import org.junit.jupiter.api.Test;
+
+public class RegisteredItemTest {
+    @Test
+    void testGetPriceOfMultipleItems() {
+
+    }
+
+    @Test
+    void testGetVatOfMultipleItems() {
+
+    }
+
+    @Test
+    void testIdsAreEqual() {
+
+    }
+
+    @Test
+    void testSetQuantity() {
+
+    }
+}
