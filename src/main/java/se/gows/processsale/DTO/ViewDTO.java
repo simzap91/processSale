c9c8516576cf60c9a911e7b6d98ab1e5a73a459c
simzap91 <115180756+simzap91@users.noreply.github.com>
2025-05-02 14:44:59 +0200

    Update ViewDTO.java

--- src/main/java/se/gows/processsale/DTO/ViewDTO.java
+++ src/main/java/se/gows/processsale/DTO/ViewDTO.java
@@ -2,6 +2,11 @@ package se.gows.processsale.DTO;
 
 import se.gows.processsale.model.RegisteredItem;
 
+/**
+     * View DTO. Holds information about last scanned item and running total of the sale.
+     * @param regItem last scanned item
+     * @param runningTotalIncVat current running total inc vat
+     */
 public class ViewDTO {
     public RegisteredItem regItem;
     public double runningTotalIncVat;
