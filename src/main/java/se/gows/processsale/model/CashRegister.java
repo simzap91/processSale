534da501d534339a5b67c4b5ab56d80c1965dd13
Sparfbag <wilmer.thorn@gmail.com>
2025-05-02 16:21:59 +0200

    Kommentarer till cashregister och discountDB

--- src/main/java/se/gows/processsale/model/CashRegister.java
+++ src/main/java/se/gows/processsale/model/CashRegister.java
@@ -4,13 +4,19 @@ public class CashRegister {
     public Transaction trans;
     public Receipt receipt;
     public Printer printer;
-
+/**
+ * A CashRegister object that contains the information about the transaction and the receipt, and stores it in controller
+ * @param trans customers payements and total price of sale, stores as a Transaction object
+ * @param receipt All information about the current sale, including a updated total price from dicounts.
+ */
     public CashRegister (Transaction trans, Receipt receipt) {
         this.trans = trans;
         this.receipt = receipt;
         this.printer = new Printer();
     }
-
+/**
+ * Public method that prints the receipt
+ */
     public void printReceipt(){
         this.printer.printReceipt(receipt);
     }
