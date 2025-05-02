f3898c976c782cbe3d839b334f9d5d37446be947
simzap91 <115180756+simzap91@users.noreply.github.com>
2025-05-02 14:33:09 +0200

    accDB test update

--- src/test/java/se/gows/processsale/integration/AccountingDBHandlerTest.java
+++ src/test/java/se/gows/processsale/integration/AccountingDBHandlerTest.java
@@ -1,59 +1,45 @@
 package se.gows.processsale.integration;
 
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Test;
 
-import se.gows.processsale.DTO.ItemDTO;
 import se.gows.processsale.DTO.SaleDTO;
-import se.gows.processsale.model.Amount;
-import se.gows.processsale.model.Receipt;
-import se.gows.processsale.model.Transaction;
+import se.gows.processsale.model.*;
 
 public class AccountingDBHandlerTest {
-    private Amount testAmountPaid;
-    private SaleDTO testSaleDTO;
-    private Transaction testTransaction;
-    private Receipt testReceipt;
     private AccountingDBHandler accDBtest;
     private ByteArrayOutputStream printoutBuffer;
     private PrintStream originalSysOut;
-    private ItemDTO testItemDTO;
 
-@BeforeEach
-public void setUp(){
-    testAmountPaid = new Amount(100);   
-    testSaleDTO  = new SaleDTO(null, 80, 20, null);
-    testTransaction = new Transaction(testAmountPaid, 100);
-    testReceipt = new Receipt(testSaleDTO, testTransaction);
-    accDBtest = new AccountingDBHandler();
-    printoutBuffer = new ByteArrayOutputStream();
-    PrintStream inMemSysOut = new PrintStream(printoutBuffer);
-    originalSysOut = System.out;
-    System.setOut(inMemSysOut);
-    
-}
-@AfterEach
-public void tearDown(){
-    testAmountPaid = null;
-    testSaleDTO = null;
-    testTransaction = null;
-    testReceipt = null;
-    accDBtest = null;
-    printoutBuffer = null;
-    System.setOut(originalSysOut);
-}
+    @BeforeEach
+    public void setUp(){
+        printoutBuffer = new ByteArrayOutputStream();
+        PrintStream inMemSysOut = new PrintStream(printoutBuffer);
+        originalSysOut = System.out;
+        System.setOut(inMemSysOut);
+    }
+
+    @AfterEach
+    public void tearDown(){
+        accDBtest = null;
+        printoutBuffer = null;
+        System.setOut(originalSysOut);
+    }
 
     @Test
-    void testUpdateAccountBalance(Receipt testReceipt) {
+    void testUpdateAccountBalance() {
+
+        Amount testAmountPaid = new Amount(100);   
+        SaleDTO testSaleDTO  = new SaleDTO(null, 80, 20, null);
+        Transaction testTransaction = new Transaction(testAmountPaid, 100);
+        Receipt testReceipt = new Receipt(testSaleDTO, testTransaction);
+        accDBtest = new AccountingDBHandler();
+
         accDBtest.updateAccountBalance(testReceipt);
         String printout = printoutBuffer.toString();
         String expectedOutput = "New balance: 200";
