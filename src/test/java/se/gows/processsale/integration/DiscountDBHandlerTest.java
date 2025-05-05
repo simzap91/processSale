7e80f40f16c526a51e11b92c4b9daefa92b8e521
Oscar Dalne <oscar.dalne@gmail.com>
2025-05-05 12:42:17 +0200

    Update DiscountDBHandlerTest.java

--- src/test/java/se/gows/processsale/integration/DiscountDBHandlerTest.java
+++ src/test/java/se/gows/processsale/integration/DiscountDBHandlerTest.java
@@ -8,7 +8,9 @@ import se.gows.processsale.DTO.ItemDTO;
 import se.gows.processsale.model.RegisteredItem;
 
 public class DiscountDBHandlerTest {
-    
+    /*
+     * Test for all discounts
+     */
     @Test
     void testGetDiscountedPriceAllDiscounts() {
         DiscountDBHandler discHandler = new DiscountDBHandler();
@@ -29,6 +31,9 @@ public class DiscountDBHandlerTest {
         assertEquals(175.95, discountedPrice, 0.01, "Discount is not calculated as expected");
     }
 
+    /*
+     * Test for no discounts
+     */
     @Test
     void testGetDiscountedPriceNoDiscountApplied() {
         DiscountDBHandler discHandler = new DiscountDBHandler();
@@ -44,5 +49,65 @@ public class DiscountDBHandlerTest {
         double discountedPrice = discHandler.getDiscountedPrice(discountTypes, customerID, purchasedItems, totalPrice);
 
         assertEquals(100.0, discountedPrice, 0.01, "Discount is not calculated as expected");
+    }
+
+    /*
+     * Test for type one discount
+     */
+    @Test
+    void testGetDiscountedPriceTypeOneDiscountApplied() {
+        DiscountDBHandler discHandler = new DiscountDBHandler();
+
+        ItemDTO item = new ItemDTO(1, "Discounted Item", 100.0, 0.25);
+        RegisteredItem regItem = new RegisteredItem(item, 1);
+
+        RegisteredItem[] purchasedItems = { regItem };
+        int[] discountTypes = { 1 }; 
+        int customerID = 99; 
+        double totalPrice = 100.0;
+
+        double discountedPrice = discHandler.getDiscountedPrice(discountTypes, customerID, purchasedItems, totalPrice);
+
+        assertEquals(80.0, discountedPrice, 0.01, "Discount for type one is not calculated as expected");
+    }
+
+    /*
+     * Test for type two discount
+     */
+    @Test
+    void testGetDiscountedPriceTypeTwoDiscountApplied() {
+        DiscountDBHandler discHandler = new DiscountDBHandler();
+
+        ItemDTO item = new ItemDTO(99, "Non-discounted Item", 300.0, 0.25);
+        RegisteredItem regItem = new RegisteredItem(item, 1);
+
+        RegisteredItem[] purchasedItems = { regItem };
+        int[] discountTypes = { 2 }; 
+        int customerID = 1; 
+        double totalPrice = 300.0;
+
+        double discountedPrice = discHandler.getDiscountedPrice(discountTypes, customerID, purchasedItems, totalPrice);
+
+        assertEquals(270.0, discountedPrice, 0.01, "Discount is not calculated as expected");
+    }
+
+    /*
+     * Test for type three discount
+     */
+    @Test
+    void testGetDiscountedPriceTypeThreeDiscountApplied() {
+        DiscountDBHandler discHandler = new DiscountDBHandler();
+
+        ItemDTO item = new ItemDTO(99, "Non-discounted Item", 100.0, 0.25);
+        RegisteredItem regItem = new RegisteredItem(item, 1);
+
+        RegisteredItem[] purchasedItems = { regItem };
+        int[] discountTypes = {3}; 
+        int customerID = 1; 
+        double totalPrice = 100.0;
+
+        double discountedPrice = discHandler.getDiscountedPrice(discountTypes, customerID, purchasedItems, totalPrice);
+
+        assertEquals(85.0, discountedPrice, 0.01, "Discount is not calculated as expected");
     }
 }
