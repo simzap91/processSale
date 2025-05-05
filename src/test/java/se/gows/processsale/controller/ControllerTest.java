68bb3199014b40e95d16401f0a98e8cd024420c3
Sparfbag <wilmer.thorn@gmail.com>
2025-05-05 11:55:08 +0200

    ControllerTest

--- src/test/java/se/gows/processsale/controller/ControllerTest.java
+++ src/test/java/se/gows/processsale/controller/ControllerTest.java
@@ -49,11 +49,11 @@ public class ControllerTest {
         ViewDTO resultItemNotRegistered = instanceToTest.scanItem(2, 2);
 
         assertNotNull(resultItemNotRegistered, "ViewDTO not created as expected");
-        assertEquals("Smör", resultItemNotRegistered.getRegItem().item.getItemDescription(), "Item description not as expected.");
-        assertEquals(2, resultItemNotRegistered.getRegItem().quantity, "Not correct item quantity.");
+        assertEquals("Smör", resultItemNotRegistered.getRegItem().getItem().getItemDescription(), "Item description not as expected.");
+        assertEquals(2, resultItemNotRegistered.getRegItem().getQuantity(), "Not correct item quantity.");
 
         ViewDTO resultItemAlreadyRegistered = instanceToTest.scanItem(2, 2);
-        assertEquals(4, resultItemAlreadyRegistered.getRegItem().quantity, "Item quantity not updated correctly.");
+        assertEquals(4, resultItemAlreadyRegistered.getRegItem().getQuantity(), "Item quantity not updated correctly.");
     }
 
     @Test
