825d41c09860be10c226b670a1f6981cd16bfdb3
simzap91 <115180756+simzap91@users.noreply.github.com>
2025-05-15 11:01:14 +0200

    comments singleton

--- src/main/java/se/gows/processsale/integration/Inventory/InventoryDb.java
+++ src/main/java/se/gows/processsale/integration/Inventory/InventoryDb.java
@@ -2,6 +2,10 @@ package se.gows.processsale.integration.Inventory;
 
 import java.util.ArrayList;
 
+/**
+ * Singelton class that represents the inventory database.
+ * When initialized it adds the three test products "mjölk", "smör" and "choklad" to the inventory.
+ */
 public class InventoryDb {
     private static final InventoryDb instance = new InventoryDb();
     private ArrayList<InventoryItem> items;
@@ -12,6 +16,14 @@ public class InventoryDb {
         items.add(new InventoryItem(2, "Smör", 39.90, 0.25));
         items.add(new InventoryItem(3, "Choklad", 9.90, 0.25));
     };
+    /**
+     * Public method that returns the only instance of this class.
+     * @return instance - the only instance of this class.
+     */
     public static InventoryDb getInstance(){return instance;}
+    /**
+     * Public method that returns a list of the inventory items.
+     * @return items - a list of all items present in inventory.
+     */
     public ArrayList<InventoryItem> getItems(){return items;}
 }
