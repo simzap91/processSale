0d231d27eae7c860cb3615a5df7bd2c082e89518
Sparfbag <wilmer.thorn@gmail.com>
2025-05-15 14:09:40 +0200

    Kommentarer för exceptions

--- src/main/java/se/gows/processsale/integration/DatabaseFailureException.java
+++ src/main/java/se/gows/processsale/integration/DatabaseFailureException.java
@@ -1,7 +1,11 @@
 package se.gows.processsale.integration;
-
+/**
+ * Thrown when program is unable to contact the database.
+ */
 public class DatabaseFailureException extends Exception {
-
+/**
+ * Constructs a new exception with a message specifying that the database cannot be reached.
+ */
     public DatabaseFailureException() {
         super("Database can not be called.");
     }
--- src/main/java/se/gows/processsale/integration/ItemIdNotFoundException.java
+++ src/main/java/se/gows/processsale/integration/ItemIdNotFoundException.java
@@ -1,10 +1,21 @@
 package se.gows.processsale.integration;
 
+/**
+ * Thrown when search is unsuccessful in finding a specified item ID
+ */
 public class ItemIdNotFoundException extends Exception {
     private int itemIdNotFound;
+
+    /**
+     * Creates a new instance of itemID together with a message clarifying which item ID the search failed to find.
+    * @param itemID the item ID that was not found
+     */
     public ItemIdNotFoundException(int itemId){
         super("Item with id " + itemId + " can't be found in inventory.\n");
         this.itemIdNotFound = itemId;
     }
+    /**
+     * @return The item ID that could not be found.
+     */    
     public int getItemIdNotFound(){return this.itemIdNotFound;}
 }
